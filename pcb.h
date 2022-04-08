dcc289ed650281d3fb0224b0ac11360b1b1f87e5
aetdc <66661765+aetdc@users.noreply.github.com>
2022-04-07 23:17:27 -0400

    Add files via upload
    added page table array to pcb struct, along with programCounter and a pageTableIndex

--- pcb.h
+++ pcb.h
@@ -5,19 +5,29 @@
  * Struct:  PCB 
  * --------------------
  * pid: process(task) id
- * PC: program counter, stores the line that the task is executing
+ * PC: 
  * start: the first line in shell memory that belongs to this task
  * end: the last line in shell memory that belongs to this task
  * job_length_score: for EXEC AGING use only, stores the job length score
+ * 
+ * pageTable: an array that stores of each frame number in the frame store
+ * pageTableIndex: tracks which line inside the frame the pcb is executing
+ * programCounter: tracks which lines of code that we're executing in a specific frame
  */
 typedef struct
 {
     char* pid;
     int PC;
-    int start;
-    int end;
+    int start; // not contiguous so not needed
+    int end; // not contiguous so not needed
     int job_length_score;
+
+    int pageTable[12]; // page table array
+    int pageTableIndex;
+    int programCounter;
 }PCB;
 
 PCB * makePCB(int start, int end, char* pid);
 #endif
+
+
