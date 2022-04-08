85c005cbc0ae40776bccee5196af23ccd186654f
aetdc <66661765+aetdc@users.noreply.github.com>
2022-04-07 23:15:19 -0400

    Add files via upload

--- pcb.c
+++ pcb.c
@@ -7,11 +7,24 @@
 
 //In this implementation, Pid is the same as file ID 
 PCB* makePCB(int start, int end, char* pid){
+
     PCB * newPCB = malloc(sizeof(PCB));
+
+    for (int i = 0; i<10; i++){
+    
+    newPCB->pageTable[i] = -1; // initialize pageTable entries to -1 for empty
+    
+    }
+
     newPCB->pid = pid;
     newPCB->PC = start;
     newPCB->start  = start;
     newPCB->end = end;
     newPCB->job_length_score = 1+end-start;
+
+    newPCB->pageTableIndex = 0;
+    newPCB->programCounter = 0;
+
     return newPCB;
+    
 }
